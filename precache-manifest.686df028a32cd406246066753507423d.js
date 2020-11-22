ac1652146d720a379aaa5f9c36287f6d3e4a0077
fatimabenzaoui <fatima.benzaoui@yahoo.fr>
2020-11-22 18:27:08 +0100

    Updates

---
+++ precache-manifest.686df028a32cd406246066753507423d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d08c2246c316469d301c0d375832345a",
+    "url": "/myReactApp/index.html"
+  },
+  {
+    "revision": "ca9dc77d7fa6d1918535",
+    "url": "/myReactApp/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "18af4372c5b71caa3fa4",
+    "url": "/myReactApp/static/css/main.93398bf0.chunk.css"
+  },
+  {
+    "revision": "ca9dc77d7fa6d1918535",
+    "url": "/myReactApp/static/js/2.d428ec6d.chunk.js"
+  },
+  {
+    "revision": "b99f8770236022defdce9d8ab079fa39",
+    "url": "/myReactApp/static/js/2.d428ec6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18af4372c5b71caa3fa4",
+    "url": "/myReactApp/static/js/main.dbd5d0c3.chunk.js"
+  },
+  {
+    "revision": "d92907550e9a7ec3bd7d",
+    "url": "/myReactApp/static/js/runtime-main.d7dac6d1.js"
+  }
+]);
